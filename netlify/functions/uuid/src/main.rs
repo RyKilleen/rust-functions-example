736e3ebbb5fe359fac42672b95abbc9e6fea9b68
Ryan Killeen <rykilleen@gmail.com>
2021-10-23 22:15:59 -0400

    refactor to allow for json array of uuids

--- netlify/functions/uuid/src/main.rs
+++ netlify/functions/uuid/src/main.rs
@@ -2,36 +2,42 @@ use aws_lambda_events::encodings::Body;
 use aws_lambda_events::event::apigw::{ApiGatewayProxyRequest, ApiGatewayProxyResponse};
 use http::header::HeaderMap;
 use lambda_runtime::{handler_fn, Context, Error};
-use log::LevelFilter;
-// use simple_logger::SimpleLogger;
+use serde_json;
+use tokio;
 use uuid::Uuid;
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-    // SimpleLogger::new()
-    //     .with_level(LevelFilter::Info)
-    //     .init()
-    //     .unwrap();
-
     let func = handler_fn(my_handler);
     lambda_runtime::run(func).await?;
     Ok(())
+}
+
+fn multiple_uuids(count: i32) -> String {
+    let uuids: Vec<_> = (0..count).into_iter().map(|_| Uuid::new_v4()).collect();
+    serde_json::to_string(&uuids).expect("dang")
 }
 
 pub(crate) async fn my_handler(
     event: ApiGatewayProxyRequest,
     _ctx: Context,
 ) -> Result<ApiGatewayProxyResponse, Error> {
-   
-        let path = event.path.unwrap();
-        let my_uuid = Uuid::new_v4();
-        let resp = ApiGatewayProxyResponse {
-            status_code: 200,
-            headers: HeaderMap::new(),
-            multi_value_headers: HeaderMap::new(),
-            // body: Some(Body::Text(format!("Hello from '{}'", path))),
-            body: Some(Body::Text(format!("{}", my_uuid))),
-            is_base64_encoded: Some(false),
-        };
-        Ok(resp)
+    let uuid_string: String;
+    let has_count = event.query_string_parameters.contains_key("count");
+    if has_count {
+        let count: i32 = event.query_string_parameters["count"]
+            .parse()
+            .expect("aww shucks");
+        uuid_string = multiple_uuids(count)
+    } else {
+        uuid_string = Uuid::new_v4().to_string()
+    }
+    let resp = ApiGatewayProxyResponse {
+        status_code: 200,
+        headers: HeaderMap::new(),
+        multi_value_headers: HeaderMap::new(),
+        body: Some(Body::Text(format!("{}", uuid_string))),
+        is_base64_encoded: Some(false),
+    };
+    Ok(resp)
 }
